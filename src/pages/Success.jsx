b7c5b2a752cf3502be295fba7398b707ec1e33f1
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-07-24 02:01:33 +0530

    feat: added success page

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,11 +1,45 @@
+import styled from '@emotion/styled'
+import { Button, Stack, Typography } from '@mui/material'
 import { useLocation } from 'react-router-dom'
+
+const Container = styled(Stack)`
+    justify-content: center;
+    align-items: center;
+    gap: 1.25em;
+    height: 75vh;
+    text-align: center;
+    & > * {
+        max-width: 700px;
+    }
+`
+const Title = styled(Typography)``
+const STitle = styled(Typography)`
+    color: var(--gray-cl);
+`
+const SButton = styled(Button)`
+    width: 208px;
+    height: 44px;
+    background-color: var(--secondary-cl);
+    color: var(--primary-cl);
+    border-radius: 0;
+    &:hover {
+        background-color: var(--secondary-cl);
+    }
+`
+
 
 const Success = () => {
     const location = useLocation()
 
     console.log(location)
   return (
-    <div>Success</div>
+    <Container>
+            <Title variant="h5" component="h1">Your Order Is Completed!</Title>
+            <STitle>
+                Thank you for your order! Your order is being processed and will be completed within 3-6 hours. You will receive an email confirmation when your order is completed.
+            </STitle>
+            <SButton>Continue Shopping</SButton>
+    </Container>
   )
 }
 
