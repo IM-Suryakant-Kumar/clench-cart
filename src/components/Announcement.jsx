c431a20e10086955cc2cb82f0e5235cc4bf6838f
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-07-25 05:22:20 +0530

    refactor: Announcement Container

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,4 +1,18 @@
-import { Container } from "../styles/announcement.css";
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+const Container = styled(Box)`
+	height: 30px;
+	background-color: var(--secondary-cl);
+	color: var(--primary-cl);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-size: 14px;
+	font-weight: 500;
+    text-align: center;
+`;
+
 
 const Announcement = () => {
   return (
