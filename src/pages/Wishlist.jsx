342a54d3bb9f72cdf6bec224c4d860939bb8136f
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-07-28 16:03:46 +0530

    feat: added wishlist page

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,10 +1,12 @@
 import { Await, defer, useLoaderData } from "react-router-dom"
-import { Container, NoItemMsg } from "../styles/wishlist.css"
+import { Bottom, Container, Image, InfoCont, NoItemMsg, Price, ProductCont, RemoveIcon, SButton, Title, Top, Wrapper } from "../styles/wishlist.css"
 import { requireAuth } from "../util"
 import { getAllwishlist } from "../api"
 import { Suspense } from "react"
 import Loader from "../components/Loader"
 import { useDispatch, useSelector } from "react-redux"
+import { addToCart } from "../features/cart/cartSlice"
+import { removeWishlist } from "../features/wishlist/wishlistSlice"
 
 export const loader = async ({ request }) => {
     await requireAuth(request)
@@ -16,13 +18,36 @@ const Wishlist = () => {
     const dispatch = useDispatch()
     const { products }  = useSelector(state => state.wishlist)
 
-    console.log(products)
+    // console.log(products)
 
     const renderProducts = () => {
         return (
             <Container>
                 <NoItemMsg length={products.length}>No items in wishlist</NoItemMsg>
-
+                <Wrapper>
+                    {products.map(prod => (
+                        <ProductCont key={prod._id}>
+                            <Top>
+                                <Image src={prod.img} />
+                                <InfoCont>
+                                    <Title>{prod.title}</Title>
+                                    <Price>Price: ₹ {prod.price}</Price>
+                                </InfoCont>
+                            </Top>
+                            <Bottom>
+                                <SButton 
+                                    variant="contained"
+                                    onClick={() => dispatch(
+                                        addToCart({productId: prod.productId})
+                                    )}
+                                >Add To Cart</SButton>
+                                <RemoveIcon onClick={() => dispatch(
+                                    removeWishlist(prod._id)
+                                )} />
+                            </Bottom>
+                        </ProductCont>
+                    ))}
+                </Wrapper>
             </Container>
         )
     }
