251a1bebfe71bb3f4d544b8b975ab5bb7aecb17c
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-08-04 11:19:24 +0530

    fix: sort products

--- src/api.js
+++ src/api.js
@@ -89,12 +89,12 @@ export const getFinalProductsData = async ( category, color, size, sort, page, s
     category && ( products = filterByCategory(products, category) )
     color && ( products = filterByColor(products, color) )
     size && ( products = filterBySize(products, size) )
-    // sort 
-    sort && ( products = sortProducts(products, sort) )
     // products length
     let length = products.length
     // Page 
     products = getProductsByPage(products, page)
+    // sort 
+    sort && ( products = sortProducts(products, sort) )
 
     return [products, filtersData, length]
 }
