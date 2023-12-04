e6b32c9ca756633ba3c6ac278b0147dea54a96a9
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-12-04 15:26:52 +0530

    feat: Updated cartThunk

--- src/features/cart/cartThunk.js
+++ src/features/cart/cartThunk.js
@@ -1,36 +1,43 @@
-import axios from "axios"
-import store from "../store"
-import { getCarts } from "./cartSlice"
+import axios from "axios";
+import store from "../store";
+import { getCarts } from "./cartSlice";
+import { getTokenFromLocalStorage } from "../../util";
 
 export const addToCartThunk = async (cart, thunkAPI) => {
-    // console.log(cart)
-    try {
-        const res = await axios.post("/api/v1/carts", cart)
-        await store.dispatch(getCarts())
-        return res.data.msg
-    } catch (err) {
-        console.log(err)
-        return thunkAPI.rejectWithValue(err.response.data.msg)
-    }
-}
+	// console.log(cart)
+	try {
+		const res = await axios.post("/api/v1/carts", cart, {
+			headers: { Authorization: `Bearer ${getTokenFromLocalStorage()}` },
+		});
+		await store.dispatch(getCarts());
+		return res.data.msg;
+	} catch (err) {
+		console.log(err);
+		return thunkAPI.rejectWithValue(err.response.data.msg);
+	}
+};
 
 export const getCartsThunk = async (cart, thunkAPI) => {
-    try {
-        const res = await axios.get("/api/v1/carts")
-        return res.data
-    } catch (err) {
-        console.log(err)
-        return thunkAPI.rejectWithValue(err.response.data.msg)
-    }
-}
+	try {
+		const res = await axios.get("/api/v1/carts", {
+			headers: { Authorization: `Bearer ${getTokenFromLocalStorage()}` },
+		});
+		return res.data;
+	} catch (err) {
+		console.log(err);
+		return thunkAPI.rejectWithValue(err.response.data.msg);
+	}
+};
 
 export const removeCartThunk = async (cart, thunkAPI) => {
-    try {
-        const res = await axios.delete(`/api/v1/carts/${cart}`)
-        await store.dispatch(getCarts())
-        return res.data.msg
-    } catch (err) {
-        console.log(err)
-        return thunkAPI.rejectWithValue(err.response.data.msg)
-    }
-}
+	try {
+		const res = await axios.delete(`/api/v1/carts/${cart}`, {
+			headers: { Authorization: `Bearer ${getTokenFromLocalStorage()}` },
+		});
+		await store.dispatch(getCarts());
+		return res.data.msg;
+	} catch (err) {
+		console.log(err);
+		return thunkAPI.rejectWithValue(err.response.data.msg);
+	}
+};
