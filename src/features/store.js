87b03d87cc9335934d78e2bdaf5e4bf71f994719
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-02-17 17:23:56 +0530

    feat: Added auth apiSLice

--- src/features/store.js
+++ src/features/store.js
@@ -1,18 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cart/cartSlice";
 import userReducer from "./user/userSlice";
-import productReducer from "./product/productSlice"
-import orderReducer from "./order/orderSlice"
-import wishlistReducer from "./wishlist/wishlistSlice"
+import productReducer from "./product/productSlice";
+import orderReducer from "./order/orderSlice";
+import wishlistReducer from "./wishlist/wishlistSlice";
+import api from "./api";
 
 const store = configureStore({
 	reducer: {
-        user: userReducer,
+		user: userReducer,
 		cart: cartReducer,
-        product: productReducer,
-        order: orderReducer,
-        wishlist: wishlistReducer
-	}
+		product: productReducer,
+		order: orderReducer,
+		wishlist: wishlistReducer,
+		[api.reducerPath]: api.reducer,
+	},
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware().concat(api.middleware),
 });
 
 export default store;
