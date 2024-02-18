4ed3c7fb3b10da1739d5caad6db53c28baa1cd4a
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-02-18 18:12:42 +0530

    refactor: updated App

--- src/App.jsx
+++ src/App.jsx
@@ -15,70 +15,11 @@ import {
 	ProductList,
 	Profile,
 	Register,
-  Success,
-  Wishlist,
+	Success,
+	Wishlist,
 } from "./pages";
-// import Layout, { loader as layoutLoader } from "./components/Layout";
-// import Home, { loader as homeLoader } from "./pages/Home";
-// import ProductList, { loader as productListLoader } from "./pages/ProductList";
-// import Product, { loader as productLoader } from "./pages/Product";
-// import Register, {
-// 	loader as registerLoader,
-// 	action as registerAction,
-// } from "./pages/Register";
-// import Login, {
-// 	loader as loginLoader,
-// 	action as loginAction,
-// } from "./pages/Login";
-// import Cart, { loader as cartLoader } from "./pages/Cart";
-// import Success, { loader as successLoader } from "./pages/Success";
-// import Profile, { loader as profileLoader } from "./pages/Profile";
-// import NotFound from "./pages/NotFound";
-// import Orders, { loader as ordersLoader } from "./pages/Orders";
-// import Wishlist, { loader as WishlistLoader } from "./pages/Wishlist";
-// import Error from "./components/Error";
 
 const App = () => {
-	// const router = createBrowserRouter(
-	// 	createRoutesFromElements(
-	// 		<Route path="/" element={<Layout />} loader={layoutLoader}>
-	// 			<Route index element={<Home />} loader={homeLoader} />
-	// 			<Route
-	// 				path="products"
-	// 				element={<ProductList />}
-	// 				loader={productListLoader}
-	// 			/>
-	// 			<Route
-	// 				path="product/:id"
-	// 				element={<Product />}
-	// 				loader={productLoader}
-	// 			/>
-	// 			<Route
-	// 				path="cart"
-	// 				element={<Cart />}
-	// 				loader={cartLoader}
-	// 				// errorElement={<Error />}
-	// 			/>
-	// 			<Route path="success" element={<Success />} loader={successLoader} />
-	// 			<Route path="orders" element={<Orders />} loader={ordersLoader} />
-	// 			<Route path="wishlist" element={<Wishlist />} loader={WishlistLoader} />
-	// 			<Route
-	// 				path="login"
-	// 				element={<Login />}
-	// 				loader={loginLoader}
-	// 				action={loginAction}
-	// 			/>
-	// 			<Route
-	// 				path="register"
-	// 				element={<Register />}
-	// 				loader={registerLoader}
-	// 				action={registerAction}
-	// 			/>
-	// 			<Route path="profile" element={<Profile />} loader={profileLoader} />
-	// 		</Route>
-	// 	)
-	// );
-
 	const router = createBrowserRouter(
 		createRoutesFromElements(
 			<Route path="/" element={<Layout />}>
@@ -87,10 +28,10 @@ const App = () => {
 				<Route path="product/:id" element={<Product />} />
 				<Route element={<HostLayout />}>
 					<Route path="cart" element={<Cart />} />
-          <Route path="wishlist" element={<Wishlist />} />
-          <Route path="orders" element={<Orders />} />
-          <Route path="success" element={<Success />} />
-          <Route path="profile" element={<Profile />} />
+					<Route path="wishlist" element={<Wishlist />} />
+					<Route path="orders" element={<Orders />} />
+					<Route path="success" element={<Success />} />
+					<Route path="profile" element={<Profile />} />
 				</Route>
 				<Route element={<AuthLayout />}>
 					<Route path="login" element={<Login />} />
