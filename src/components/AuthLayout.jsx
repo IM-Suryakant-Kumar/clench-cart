24a4b294a7894ce69004895611608a5ab436b467
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-02-18 10:21:10 +0530

    feat: added HostLayout and AuthLayout

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,7 +1,19 @@
+import { useGetProfileQuery } from "../features/apis";
+import { Loader } from ".";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
 const AuthLayout = () => {
-  return (
-    <div>AuthLayout</div>
-  );
-}
+	const { isLoading, isError } = useGetProfileQuery();
+	const state = useLocation().state;
+	const pathname = state?.redirectTo || "/";
+
+	return isLoading ? (
+		<Loader />
+	) : isError ? (
+		<Outlet />
+	) : (
+		<Navigate to={pathname} replace />
+	);
+};
 
 export default AuthLayout;
