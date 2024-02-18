138de0259f3e522ccc7697a66424fe40a4cd36f8
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-02-18 15:58:03 +0530

    refactor: updated cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,67 +1,60 @@
 import PayButton from "../components/PayButton";
-import { requireAuth } from "../util"
 import {
 	Container,
 	Summary,
 	Item,
 	Title,
-    ProductsCont,
-    ItemText,
-    ItemPrice,
-    NoItemMsg
+	ProductsCont,
+	ItemText,
+	ItemPrice,
+	NoItemMsg,
 } from "../styles/cart.css";
 import CartItem from "../components/CartItem";
-import { getAllCart } from "../api";
-import React, { Suspense } from "react";
-import { useSelector } from "react-redux";
-import { Await, defer, useLoaderData } from "react-router-dom";
 import Loader from "../components/Loader";
-
-export const loader = async ({ request }) => {
-    await requireAuth(request)
-    return  defer({products: getAllCart()})
-}
+import { useGetCartsQuery } from "../features/apis";
 
 const Cart = () => {
-    const { products, totalPrice } = useSelector(state => state.cart)
-    const loaderData = useLoaderData()
+	const { data, isLoading } = useGetCartsQuery();
+	let products, totalPrice;
+	if (data) {
+		products = data.products;
+		totalPrice = data.totalPrice;
+	}
 
-    const renderCartProducts = () => (
-        <Container>
-            <ProductsCont>
-                {products.map(prod => (
-                    <CartItem product={prod} key={prod._id} />
-                ))}
-            </ProductsCont>
-            {/* No items msg */}
-            <NoItemMsg length={products.length}>No items in cart</NoItemMsg>
-            <Summary length={products.length} elevation={1}>
-                <Title variant="body1" component="h2" className="summary">ORDER SUMMARY</Title>
-                <Item>
-                    <ItemText>Subtotal</ItemText>
-                    <ItemPrice>₹ {totalPrice}</ItemPrice>
-                </Item>
-                <Item>
-                    <ItemText>Estimated Shipping</ItemText>
-                    <ItemPrice>₹ 5.90</ItemPrice>
-                </Item>
-                <Item>
-                    <ItemText>Shipping Discount</ItemText>
-                    <ItemPrice>₹ -5.90</ItemPrice>
-                </Item>
-                <Item type="total">
-                    <ItemText>Total </ItemText>
-                    <ItemPrice>₹ {totalPrice}</ItemPrice>
-                </Item>
-                <PayButton products={products} />
-            </Summary>
+	return isLoading ? (
+		<Loader />
+	) : (
+		<Container>
+			<ProductsCont>
+				{products.map(prod => (
+					<CartItem product={prod} key={prod._id} />
+				))}
+			</ProductsCont>
+			{/* No items msg */}
+			<NoItemMsg length={products.length}>No items in cart</NoItemMsg>
+			<Summary length={products.length} elevation={1}>
+				<Title variant="body1" component="h2" className="summary">
+					ORDER SUMMARY
+				</Title>
+				<Item>
+					<ItemText>Subtotal</ItemText>
+					<ItemPrice>₹ {totalPrice}</ItemPrice>
+				</Item>
+				<Item>
+					<ItemText>Estimated Shipping</ItemText>
+					<ItemPrice>₹ 5.90</ItemPrice>
+				</Item>
+				<Item>
+					<ItemText>Shipping Discount</ItemText>
+					<ItemPrice>₹ -5.90</ItemPrice>
+				</Item>
+				<Item type="total">
+					<ItemText>Total </ItemText>
+					<ItemPrice>₹ {totalPrice}</ItemPrice>
+				</Item>
+				<PayButton products={products} />
+			</Summary>
 		</Container>
-    )
-
-	return (
-        <Suspense fallback={<Loader />}>
-            <Await resolve={loaderData.products}>{renderCartProducts}</Await>
-        </Suspense>
 	);
 };
 
